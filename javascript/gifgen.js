236d1682cf67ef9ea7ef125895d99d6374ca144c
Microhom <microhom@gmail.com>
2015-08-25 18:25:41 +0200

    image source

--- javascript/gifgen.js
+++ javascript/gifgen.js
@@ -3,9 +3,20 @@ gifshot.createGIF({
     gifHeight: 720,
     images: [
       //choose images dinamicaly from folder?
-        'http://i.imgur.com/2OO33vX.jpg',
-        'http://i.imgur.com/qOwVaSN.png',
-        'http://i.imgur.com/Vo5mFZJ.gif'
+        
+	    '/time-lapse/timelapse0001.jpeg',
+		'/time-lapse/timelapse0002.jpeg',
+		'/time-lapse/timelapse0003.jpeg',
+		'/time-lapse/timelapse0004.jpeg',
+		'/time-lapse/timelapse0005.jpeg',
+		'/time-lapse/timelapse0006.jpeg',
+        '/time-lapse/timelapse0007.jpeg', 
+        '/time-lapse/timelapse0008.jpeg',
+        '/time-lapse/timelapse0009.jpeg',
+        '/time-lapse/timelapse0010.jpeg',
+        '/time-lapse/timelapse0011.jpeg',
+        '/time-lapse/timelapse0012.jpeg',
+        '/time-lapse/timelapse0013.jpeg',
     ],
     numFrames: 1000
 }, function (obj) {
